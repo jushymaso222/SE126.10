6a4077f7cf51c740ac8b4302930ba7b58a9b7a13
Joshua Mason <joshuamason418@gmail.com>
2024-03-02 14:19:38 -0500

    lab8

--- week8/lab8.py
+++ week8/lab8.py
@@ -1,7 +1,23 @@
-import csv
+#  Lab 8 - Joshua Mason
+#
+#  Seat selection and purchasing program for a large theater
+# -----------------------------------------------------------
+#  Variable Dictionary:
+#     seatMap - the dictionary which contains all seat information (usually pulled from the seatMap.yaml file)
+#     alpha - the acceptable values for seats; contains the full alphabet and 1-4 for column names
+#     printMap() - prints the seat map to the console
+#     load() - loads the data from the file and creates a file with default values if not found
+#     save() - saves all data from the seatMap list to the file
+#     calculateTotal() - runs through the dictionary and calculates the total of seats sold by type and overall price
+#     calculateSeats() - runs through the dictionary and calculates the total number of seats available by row and overall seats
+#     selectSeat() - selects seats to add to your cart for purchasing later
+#     menu() - recursive loop to get users input and run through available program functions
+# -----------------------------------------------------------
+
+#Imports
 import os
-import time
-import yaml
+import time #For stopping time like a superhero
+import yaml #For data storage in files
 
 rows = 15 #Number of rows on the plane
 cols = 29 #Number of columns on the plane
@@ -35,7 +51,7 @@ def load():
                 seatMap[str(i+1)][letter] = "#" #Sets the default value to the letter from the alphabet that corresponds with the given index
 
         time.sleep(5)
-        f = open("week8/seatMap.yaml","wt")
+        f = open("week8/seatMap.yaml","wt") #The 'wt' stands for write
         yaml.dump(seatMap, f)
 
         f = open("week8/seatMap.yaml","r")
@@ -43,48 +59,48 @@ def load():
     return yaml.full_load(f)
 
 def save(list):
-    f = open("week8/seatMap.yaml","wt")
+    f = open("week8/seatMap.yaml","wt") #Saves the list to the file
     yaml.dump(list, f)
 
 def calculateTotal():
-    total = 0
-    totals = [0, 0, 0]
+    total = 0 #Overall total cost of seats
+    totals = [0, 0, 0] #Total cost of seats by class
 
     for row in seatMap:
         for col in seatMap[row]:
-            if seatMap[row][col] == "*":
-                if int(row) < 6:
+            if seatMap[row][col] == "*": #Checks each value in the list for an '*' and if found performs the following
+                if int(row) < 6: #Rows 1-5 are $200
                     total += 200
                     totals[0] += 200
-                elif int(row) < 11:
+                elif int(row) < 11: #Rows 6-10 are $175
                     total += 175
                     totals[1] += 175
-                else:
+                else: #All others higher than 11 are $150
                     total += 150
                     totals[2] += 150
     
-    return total, totals
+    return total, totals #Returns both totals variables
 
-def calculateSeats():
-    rowSeats = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    totalSeats = 0
-    sold = 0
+def calculateSeats(): #Calculates the available seats
+    rowSeats = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] #List of all available seats by row
+    totalSeats = 0 #Overall total seats
+    sold = 0 #How many seats have been sold
 
     for row in seatMap:
         for col in seatMap[row]:
-            if seatMap[row][col] == "#":
-                rowSeats[int(row)-1] += 1
-                totalSeats += 1
+            if seatMap[row][col] == "#": #Checks all values in the dictionary for a '#' then runs the following
+                rowSeats[int(row)-1] += 1 #Adds a count to the respective row in the list
+                totalSeats += 1 #Adds a count to the overall total
             else:
                 sold += 1
 
-    return rowSeats, totalSeats, sold
+    return rowSeats, totalSeats, sold #Returns all three variables
 
 
 
-selectedSeats = []
+selectedSeats = [] #Initializing a list for the selected seats
 
-def selectSeat():
+def selectSeat(): #Select seats to add to cart
     selecting = "y"
     while selecting != "n":
         os.system('cls')
@@ -120,12 +136,12 @@ def selectSeat():
             time.sleep(2)
 
         selecting = input("\nWould you like to reserve another seat? [y / n]")
-        while selecting != "y" and selecting != "n":
+        while selecting != "y" and selecting != "n": #Trap loop for users giving incorrect responses
             selecting = input("Incorrect input. Would you like to reserve another seat? [y / n]")
 
 
 
-def menu():
+def menu(): #Menu function 
     os.system('cls')
 
     printMap()
@@ -147,36 +163,37 @@ def menu():
     if answer != -1:
         return answer
     else:
-        return menu()
+        return menu() #Recursive trap loop :)
     
 
-seatMap = load()
+seatMap = load() #Loads the file (if the file does not exist, it will create a new one following gthe default values)
 
 selection = -1    
 while selection != 5:
-    selection = menu()
+    selection = menu() #Grab the users menu selection
 
     if selection == 1:
-        selectSeat()
+        selectSeat() #Add seats to cart
     elif selection == 2:
         os.system('cls')
-        total, totals = calculateTotal()
+        total, totals = calculateTotal() #Gets the totals for all sales
         print(f"Sales:\n\tPremium Seats: ${totals[0]:.2f}\n\tRegular Seats: ${totals[1]:.2f}\n\tEconomy Seats: ${totals[2]:.2f}\n\nTotal: ${total:.2f}")
         input("\nPress enter to continue...")
     elif selection == 3:
         os.system('cls')
-        rows, total, sold = calculateSeats()
+        rows1, total, sold = calculateSeats() #Gets the total available seats from each row
         print("Available Seats:")
-        print(f"\tRow 1: {rows[0]}\n\tRow 2: {rows[1]}\n\tRow 3: {rows[2]}\n\tRow 4: {rows[3]}\n\tRow 5: {rows[4]}\n\tRow 6: {rows[5]}\n\tRow 7: {rows[6]}\n\tRow 8: {rows[7]}\n\tRow 9: {rows[8]}\n\tRow 10: {rows[9]}\n\tRow 11: {rows[10]}\n\tRow 12: {rows[11]}\n\tRow 13: {rows[12]}\n\tRow 14: {rows[13]}\n\tRow 15: {rows[14]}")
+        print(f"\tRow 1: {rows1[0]}\n\tRow 2: {rows1[1]}\n\tRow 3: {rows1[2]}\n\tRow 4: {rows1[3]}\n\tRow 5: {rows1[4]}\n\tRow 6: {rows1[5]}\n\tRow 7: {rows1[6]}\n\tRow 8: {rows1[7]}\n\tRow 9: {rows1[8]}\n\tRow 10: {rows1[9]}\n\tRow 11: {rows1[10]}\n\tRow 12: {rows1[11]}\n\tRow 13: {rows1[12]}\n\tRow 14: {rows1[13]}\n\tRow 15: {rows1[14]}")
         print(f"\nTotal Seats Available: {total}")
         print(f"\nTotal Seats Sold: {sold}")
         input("\nPress enter to continue...")
     elif selection == 4:
+        os.system('cls')
         print("\nHere are the seats you have reserved for:")
-        totalPrice = 0
+        totalPrice = 0 #Total price of all seats in the selectedSeats list
         for i in range(0, len(selectedSeats)): #Prints all the values in the seatsSelected list
-            rowNum = int(selectedSeats[i][:-1])
-            if rowNum < 6:
+            rowNum = int(selectedSeats[i][:-1]) #Removes the letter from the seat number to get just the row number and checks it against a few values
+            if rowNum < 6: #This is the same total generator as above just for the selectedSeats list
                 price = 200
                 totalPrice += 200
             elif rowNum < 11:
@@ -190,14 +207,15 @@ while selection != 5:
         print(f"\nTotal: ${totalPrice:.2f}")
 
         answer = "x"
-        while answer != "y" and answer != "n":
+        while answer != "y" and answer != "n": #Basic user trap loop for purchase selection
             answer = input("Would you like to purchase these tickets? [y / n]")
             if answer != "y" and answer != "n":
                 print("Invalid response!")
 
-        if answer == "y":
+        if answer == "y": #If seats are purchased, all selected seats are saved to the file
             save(seatMap)
             print("Purchased successfully!")
+            selectedSeats = [] #Resets the purchased seats list so you can't purchase the same seats again
         else:
             answer1 = "x"
             while answer1 != "y" and answer1 != "n":
@@ -206,7 +224,8 @@ while selection != 5:
                     print("Invalid response!")
 
             if answer1 == "y":
-                seatMap = load()
+                seatMap = load() #Reloads the file to restore the values from before selection began
+                selectedSeats = [] #Clears the selected seats list
                 print("Seat selection cleared!")
 
         time.sleep(3)
@@ -215,4 +234,4 @@ while selection != 5:
         print("**ERROR, NOT A VALID SELECTION!**")
         time.sleep(1)
     
-print("Thank you for your time!")
+print("Thank you for your time!") #End of program
